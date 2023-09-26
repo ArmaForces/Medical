d4886b713f7401a3a9ee83b5822cd0ce877a42ff
Filip Maciejewski <veteran29@users.noreply.github.com>
2023-09-27 01:27:14 +0200

    Update ACE Arsenal integration for ACE 3.16.0

--- addons/medical_treatment/CfgWeapons.hpp
+++ addons/medical_treatment/CfgWeapons.hpp
@@ -4,12 +4,17 @@ class CfgWeapons {
     class AFMED_apap: ACE_ItemCore {
         scope = 2;
         scopeCurator = 2;
+
         author = "ArmaForces";
         displayName = CSTRING(Apap_DisplayName);
-        picture = QPATHTOF(ui\icons\apap.paa);
-        model = "\A3\Structures_F_EPA\Items\Medical\PainKillers_F.p3d";
         descriptionShort = CSTRING(Apap_Desc_Short);
         descriptionUse = CSTRING(Apap_Desc_Use);
+        picture = QPATHTOF(ui\icons\apap.paa);
+
+        model = "\A3\Structures_F_EPA\Items\Medical\PainKillers_F.p3d";
+
+        ACE_isMedicalItem = 1;
+
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.5;
         };
